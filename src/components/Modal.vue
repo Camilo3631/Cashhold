b98091632d3830d6f378cca1dae7502fa9671955
Camilo Acosta <camiloacosta1193@outlook.com>
2026-01-05 20:18:24 -0500

    Revert "Merge branch 'develop-03'"
    This reverts commit 6a6ae374e33d6ce1fdbcbd224e6642df2893a899, reversing
    changes made to e7c652271cf1a08af81a63d85b963d1f3db497cf.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -55,7 +55,7 @@ const checkVisibility = () => {
   const isMobile = Math.min(width, height) < 768
 
   if (isMobile) {
-    showModal.value = isPortrait && width >= 375
+    showModal.value = isPortrait && width >= 390
   } else {
     showModal.value = true
   }
